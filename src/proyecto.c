6018d70af70c480c8943b787c608e267327abf86
Erickescorpions <erickescorpions@hotmail.com>
2023-11-30 01:23:46 -0600

    agregando control pwm

--- src/proyecto.c
+++ src/proyecto.c
@@ -8,6 +8,7 @@
 #use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7)
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define LCD_RS_PIN      PIN_D0
 #define LCD_RW_PIN      PIN_D1
@@ -22,55 +23,37 @@
 
 #define BOTON_CAMBIO PIN_B0
 #define TAM_COMANDO 20
-// selector = 0 -> control mediante potenciometro
-// selector = 1 -> control mediante terminal   
-int selector = 0;
 
-#INT_EXT
-void interrupcion_RB0() {
-   if(input(BOTON_CAMBIO)) {
-//!      lcd_clear();
-   
-      if(selector == 1) {
-         selector = 0;
-      }
-      else {
-         selector = 1;
-      }
-   }
-   
-   delay_us(10);
-}
 
 void ajustaPWM(int16 ciclo);
 int leer_comando(char* comando);
 int separar_comando(char* comando, char* porcentaje_pwm);
+void comando_pwm(int ciclo);
+int esNumero(char* cadena);
+void limpiarLCD();
 
 int main()
-{
-   lcd_init();  
-   int valorAD;
+{ 
    char comando[TAM_COMANDO]; // mensaje recibio: pwm=100]
    char porcentaje_pwm[3] = "50";
    int recibio_comando = 0;
+   
+   // inicializamos LCD
+   lcd_init(); 
 
-   // habilitamos la interrupcion externa RB0
-   enable_interrupts(INT_EXT);
-   ext_int_edge(L_TO_H);
-   enable_interrupts(GLOBAL);
-
+   // int valorAD
    // lectura de porcentaje duty cicle mediante puerto analogico AN0
    // establecemos puerto AN0 como analogico
-   setup_adc_ports(AN0);      
+   // setup_adc_ports(AN0);      
 
    // coloca el reloj interno del micro como fuente de reloj para el mod adc
-   setup_adc(ADC_CLOCK_INTERNAL);
+   // setup_adc(ADC_CLOCK_INTERNAL);
    
    // coloca el puerto AN0 como entrada del modulo adc
-   set_adc_channel(0);
+   // set_adc_channel(0);
    
    // retardo para que se configure el modulo adc
-   delay_us(50);
+   // delay_us(50);
    
    // configuramos el modulo ccp en modo pwm
    setup_ccp1(CCP_PWM);
@@ -83,7 +66,7 @@ int main()
    setup_timer_2(T2_DIV_BY_16,155,1);   
    set_pwm1_duty(0);
    
-   int porcentaje = 0;
+   int ciclo = 0;
    
    printf("Para cambiar un control presione el boton. \n\n");
    printf("Cuando utilice el modo terminal, usar el comando: 'pwm=80'\n");
@@ -93,85 +76,68 @@ int main()
       
       if(recibio_comando) {
          int comando_correcto = separar_comando(&comando, &porcentaje_pwm);
+         limpiarLCD();
          
-         switch( comando_correcto ){
+         switch(comando_correcto) {
             case 1:
                lcd_gotoxy(1,1);
-               printf(lcd_putc," Comando temperatura \n "); 
+               printf(lcd_putc, "Com temperatura"); 
                lcd_gotoxy(1,2);
-               printf(lcd_putc," Temperatura: \n ");
-               printf("comando temperatura");
+               printf(lcd_putc, "Temperatura:");
                break;
                
             case 2:
-               lcd_gotoxy(1,1);
-               printf(lcd_putc," Comando pwm \n "); 
-               lcd_gotoxy(1,2);
-               printf(lcd_putc," Señal pwm: \n ");
-               printf("comando pwm");
+               ciclo = atol(porcentaje_pwm);
+        
+               comando_pwm(ciclo);
                break;
             case 3:
                lcd_gotoxy(1,1);
-               printf(lcd_putc," Comando motor on  \n "); 
+               printf(lcd_putc, "Com motor on"); 
                lcd_gotoxy(1,2);
-               printf(lcd_putc," Motores encendidos \n ");
-               //!               porcentaje = atol(porcentaje_pwm);
-               
-//!               if(porcentaje > 99) { 
-//!                  porcentaje = 99;
-//!               }
+               printf(lcd_putc, "Motor encendido");
                break;
             case 4:
                lcd_gotoxy(1,1);
-               printf(lcd_putc," Comando motor off  \n "); 
+               printf(lcd_putc, "Com motor off"); 
                lcd_gotoxy(1,2);
-               printf(lcd_putc," Motores apagados \n "); 
+               printf(lcd_putc, "Motores apagado"); 
                break;
                
             case 5:
                lcd_gotoxy(1,1);
-               printf(lcd_putc," Comando leds on  \n "); 
+               printf(lcd_putc, "Com leds on"); 
                lcd_gotoxy(1,2);
-               printf(lcd_putc," Leds encendidos \n "); 
+               printf(lcd_putc, "Leds encendidos"); 
                break;
                
             case 6:
                lcd_gotoxy(1,1);
-               printf(lcd_putc," Comando leds off  \n "); 
+               printf(lcd_putc, "Com leds off"); 
                lcd_gotoxy(1,2);
-               printf(lcd_putc," Leds apagados \n "); 
+               printf(lcd_putc, "Leds apagados"); 
                break;
             
             default:
                lcd_gotoxy(1,1);
-               printf(lcd_putc," Comando no reconocido  \n ");             
+               printf(lcd_putc, "Comando no");
+               lcd_gotoxy(1,2);
+               printf(lcd_putc, "reconocido");
          }
       }
+      
       delay_us(10);
-
    }                              
    
    return 0;
 }
 
-void ajustaPWM(int16 ciclo)
-{
-   if(ciclo>=0 && ciclo<=100) {
-      
-      set_pwm1_duty((int16)(ciclo * 624 / 100));
-   } else {
-      set_pwm1_duty(0);
-   }
-}
 
 int leer_comando(char* comando) {
    // se revisa si se han recibido datos desde el puerto serial
    if(kbhit()) {
       gets(comando);
-      printf("%s", comando);
-      
       return 1;
-        
    }
    
    return 0; // no recibio ningun comando
@@ -186,17 +152,17 @@ int separar_comando(char* comando, char* porcentaje_pwm) {
    
    char *numero = strtok(0,delim);
    
-   if( token ){
-      printf("%s", token);
-   
+   if(token){
       if (strcmp(token, (void*) "temperatura") == 0) {
          return 1; 
       }
          
       if (strcmp(token, (void*) "pwm") == 0) {
-         if( numero ){
-            strcpy(porcentaje_pwm, numero);
-            return 2;
+         if(numero){
+            if(esNumero(numero)) {
+               strcpy(porcentaje_pwm, numero);
+               return 2;
+            }
          }             
       }
         
@@ -219,4 +185,36 @@ int separar_comando(char* comando, char* porcentaje_pwm) {
    
    
    return 0;
+}
+
+int esNumero(char *cadena) {
+    // Iterar sobre cada caracter de la cadena
+    while (*cadena != '\0') {
+        // Verificar si el caracter no es un dígito
+        if (!isdigit(*cadena)) {
+            return 0;  // No es un número
+        }
+        cadena++;
+    }
+    return 1;  // Todos los caracteres son dígitos, es un número
+}
+
+void comando_pwm(int ciclo) {
+   lcd_gotoxy(1,1);
+   printf(lcd_putc,"Com pwm"); 
+   lcd_gotoxy(1,2);
+   printf(lcd_putc,"Senial pwm: %d", ciclo);
+   
+   if(ciclo >= 0 && ciclo <= 100) {
+      set_pwm1_duty((int16)(ciclo * 624 / 100));
+   } else {
+      set_pwm1_duty(0);
+   }
+}
+
+void limpiarLCD() {
+   lcd_gotoxy(1,1);
+   printf(lcd_putc,"                "); 
+   lcd_gotoxy(1,2);
+   printf(lcd_putc,"                "); 
 }
